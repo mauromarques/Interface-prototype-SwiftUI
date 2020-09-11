d68c7f0fe1f1814c4c7db997f7c99eea867d0798
Mauro <mmcanhao@gmail.com>
2020-09-11 09:31:18 +0100

    Initial Commit

---
+++ MeditationAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeditationAppSwiftUI
+//
+//  Created by Mauro Canhao on 11/09/2020.
+//  Copyright © 2020 Mauro Canhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
